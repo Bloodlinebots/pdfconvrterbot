b5f0861f84a1455b0a70bb3bd0f23cb2406b493f
Bloodlinebots <githubsbots@gmail.com>
2025-06-04 21:57:49 +0530

    Update filters.py

--- utils/filters.py
+++ utils/filters.py
@@ -3,7 +3,11 @@
 from telegram.ext import MessageFilter
 
 class IsImage(MessageFilter):
-    def filter(self, message):
-        return message.photo or (message.document and message.document.mime_type.startswith("image/"))
+    def filter(self, message) -> bool:
+        if message.photo:
+            return True
+        if message.document and message.document.mime_type:
+            return message.document.mime_type.startswith("image/")
+        return False
 
 is_image = IsImage()
